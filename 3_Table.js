f29745af927f29c852084f243973ef6c64f9fe7e
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-19 07:18:27 -0600

    CT: Drawing a table

--- 3_Table.js
+++ 3_Table.js
@@ -1,33 +1,38 @@
 // Draw a table
 
+// Ask the user
+
+var height = prompt('Height: ') // 100
+var width = prompt('Width: ') // 140
+
 // Draw the columns / vertical lines
 
 for (i = 0; i < 5 + 1; i++) {
     pendown()
-    forward(100)
+    forward(height)
     penup()
-    right(180)
-    forward(100)
+    left(180)
+    forward(height)
     left(90)
-    forward(140/5)
+    forward(width/5)
     left(90)
 }
 
 // Move turtle back to the starting position
 
 left(90)
-forward(140 + (140 / 5))
+forward(width + (width / 5))
 left(180)
 
 // Draw the rows / horizontal lines
 
 for (i = 0; i < 4 + 1; i++) {
     pendown()
-    forward(140)
+    forward(width)
     penup()
     right(180)
-    forward(140)
+    forward(width)
     right(90)
-    forward(100/4)
+    forward(height/4)
     right(90)
 }
