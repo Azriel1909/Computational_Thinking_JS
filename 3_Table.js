1a3a721905f4d2ee7202966eee07e236e2d36a37
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-19 07:37:08 -0600

    CT: Drawing a table

--- 3_Table.js
+++ 3_Table.js
@@ -1,17 +1,20 @@
 // Draw a table
 
-var height = Number(prompt('Table height: '))
-var width = Number(prompt('Table width: '))
+// Get key values from the user
+var height = Number( prompt( "Table height?: "))
+var width = Number( prompt( "Table width?: "))
+var cols = Number( prompt( "Table columns?: ")) 
+var rows = Number( prompt( "Table rows?: ")) 
 
 // Draw the colums / vertical lines
-for( i=0; i<6; i++ ){
+for( i=0; i<cols + 1; i++ ){
     pendown()
     forward( height )
     penup()
     right( 180 )
     forward( height )
     left( 90 )
-    forward( width / 5 )
+    forward( width / cols )
     left( 90 )
 }
 
@@ -21,13 +24,13 @@ forward( width + (width / 5) )
 left( 180 )
 
 // Draw the rows / horizontal lines
-for( i=0; i<5; i++ ){
+for( i=0; i< rows + 1; i++ ){
     pendown()
-    forward( width )
+    forward( 140 )
     penup()
     right( 180 )
-    forward( width )
+    forward( 140 )
     right( 90 )
-    forward( height / 4 )
+    forward( 100 / rows )
     right( 90 )
 }
