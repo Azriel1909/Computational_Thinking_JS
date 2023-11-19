0fd9bd2ee1b5dde3a4411370534151a93370d68b
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-18 21:44:35 -0600

    CT: Turtle in the starting position

--- 3_Table.js
+++ 3_Table.js
@@ -2,7 +2,7 @@
 
 // Draw the columns / vertical lines
 
-for (i = 0; i < 5; i++) {
+for (i = 0; i < 5 + 1; i++) {
     pendown()
     forward(100)
     penup()
@@ -13,4 +13,21 @@ for (i = 0; i < 5; i++) {
     left(90)
 }
 
+// Move turtle back to the starting position
+
+left(90)
+forward(140 + (140 / 5))
+left(180)
+
 // Draw the rows / horizontal lines
+
+for (i = 0; i < 4 + 1; i++) {
+    pendown()
+    forward(100)
+    penup()
+    right(180)
+    forward(100)
+    left(90)
+    forward(140/5)
+    left(90)
+}
