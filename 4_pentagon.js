c298aa80e9880dc410d5b50fb83a5a732a450670
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-19 08:28:50 -0600

    CT: Drawing a Pentsgon & Octagon

---
+++ 4_pentagon.js
@@ -0,0 +1,13 @@
+// Draw a pentagon
+pendown()
+for( i=0; i<7; i++ ){
+    forward( 50 )
+    right( 72 )
+}
+
+// Draw a octagon
+pendown()
+for( i=0; i<9; i++ ){   // See change here?
+    forward( 50 )
+    right( 45 )         // and here?
+}
