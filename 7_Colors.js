7f6108b4ac2a0759113e5509e9fcd6af20c29d2b
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-21 17:22:42 -0600

    CT: Drawing a tree

--- 7_Colors.js
+++ 7_Colors.js
@@ -17,3 +17,62 @@ function sun(){
 }
 
 sun()
+
+// > Draw a tree
+
+function tree() { // Function header
+  // Function body
+  right(125)
+  forward(60)
+  right(142)
+  colour(102, 51, 0)
+  width(20)
+  pendown()
+  forward(90)
+  penup()
+
+  right(90)
+  forward(5)
+  left(60)
+  width(10)
+  pendown()
+  forward(20)
+  left(45)
+  width(5)
+  forward(40)
+  left(180)
+  forward(40)
+  left(135)
+  width(5)
+  forward(30)
+  right(180)
+  forward(70)
+
+  right(60)
+  forward(10)
+  right(60)
+  width(10)
+  pendown()
+  forward(20)
+  left(45)
+  width(5)
+  forward(40)
+  left(180)
+  forward(40)
+  left(135)
+  width(9)
+  forward(20)
+  right(30)
+  width(5)
+  forward(30)
+  right(180)
+  forward(30)
+  right(150)
+  width(5)
+  forward(30)
+  right(180)
+  forward(70)
+  penup()
+}
+
+// - Main program
