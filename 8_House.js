545a2ec604b1084a5c21e753980e667180be4177
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-21 18:55:27 -0600

    CT: Drawing a house - part 1 - House base

---
+++ 8_House.js
@@ -0,0 +1,69 @@
+// * The problem: Drawing a house
+
+/**
+ * 1. flat-roofed house over two floors
+ * 2. a door in the middle of the lower floor
+ * 3. five windows
+ * 4. a chimney on the right hand side
+ */
+
+// - Developing a plan
+
+/**
+ * Start drawing a big rectangle
+ * Divide the rectangle by the half
+ * In the middle of the  rectangle (in the 1st floor) use a small rectangle to draw the door.
+ * In the top of the house (right hand side) draw a square and over thar, a rectangle (this is the chimney)
+ * Finally, use a square to draw the five windows in their respective place (we can use the same function to draw the window and the a especially sequence of instructions to indicate the place).
+ */
+
+// -- A possible plan
+
+/**
+ * Draw the walls and the roof
+ * Draw the chimney
+ * Draw the door
+ * Draw the windows
+ */
+
+// > Turn the plan into a program framework
+
+// Structure of the program
+
+// My functions
+
+function walls(){
+  // ? The empty function definition is called a Stub.
+
+  console.log('Calling the walls()')
+  // Drawing the big square
+  for( i=0; i<4; i++ ){
+    forward( 200 )
+    right( 90 )
+  }
+
+  // Divide the big square by the half
+  penup()
+  forward(200/2)
+  right(90)
+  pendown()
+  forward(200)
+
+  // Return to the beginning place of the turtle
+  penup()
+  left(180)
+  forward(200)
+  left(90)
+  forward(200/2)
+  right(180)
+}
+
+function roof(){
+  
+}
+
+// Main program
+
+walls()
+
+// Structure of the program
