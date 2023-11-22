62a74cf6d77320786f53ff199b91ee6f915c9485
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-22 07:38:16 -0600

    CT: Drawing a houses

--- 10_Parameters.js
+++ 10_Parameters.js
@@ -40,16 +40,32 @@ function drawWindows(x,y){
   drawAWindow(20)
 }
 
+function myHouse(x,y,w,h){
+  // Wall
+  myRectangle(x,y,w,h)
+  // Door
+  myRectangle(x+40,y,w/5,h/4)
+  
+  // Windows
+  drawWindows(x+0,y+30)
+  drawWindows(x+80,y+30)
+  drawWindows(x+80,y+80)
+  drawWindows(x+0,y+80)
+}
+
 // main program
 
-// Wall
-myRectangle(0 , 0, 100, 150)
+// // Wall
+// myRectangle(0 , 0, 100, 150)
 
-// Door
-myRectangle(40 , 0, 20, 50)
+// // Door
+// myRectangle(40 , 0, 20, 50)
 
-// Windows
-drawWindows(0,30)
-drawWindows(80,30)
-drawWindows(80,80)
-drawWindows(0,80)
+// // Windows
+// drawWindows(0,30)
+// drawWindows(80,30)
+// drawWindows(80,80)
+// drawWindows(0,80)
+
+myHouse(0, 0, 100, 150)
+myHouse(150, 150, 100, 150)
