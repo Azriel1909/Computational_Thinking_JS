d59a18a01fb3429f57eca2e09844062002cdc367
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-25 09:24:06 -0600

    CT: Drawing cup of coffee

--- 18_Cup.js
+++ 18_Cup.js
@@ -25,6 +25,53 @@
 
 // * Design
 
+/**
+ * Background (static) part:
+ * 1. Semicircle (where the coffee is contained)
+ * 2. The cup plate (only an horizontal straight)
+ * 
+ * Moving Part: 
+ * 1. The steam are three lines with some curves in from of waves over the cup of coffee.
+ */
+
 // * Implement
+
+// Functions
+
+function cup(x,y,r){
+  goto(x,y)
+  angle(90)
+  forward(r)
+  right(90)
+  
+  // Determine how many segments needed
+  const segments = 10 + r/1
+  const deltaAngle = 180/segments
+  const deltaLength = Math.sqrt(4-x**2)
+
+  // Drawing the circle with a lot of straight lines
+  pendown()
+  for(let i=0; i<segments; i++){
+    forward(deltaLength)
+    right(deltaAngle)
+  }
+  penup()
+}
+
+// + a = angle
+// + l = length
+
+function topOfTheCup(l,a){
+	pendown()
+	right(a)
+	forward(l)
+}
+
+// Main program
+
+penup()
+cup(0,0,100)
+topOfTheCup(130,90)
+hideTurtle()
 
 // * Test
