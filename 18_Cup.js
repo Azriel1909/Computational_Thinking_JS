2d6a438ed53f116a8945a6c4f1f268522a18c338
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-25 09:05:02 -0600

    CT: Drawing cup of coffee

---
+++ 18_Cup.js
@@ -0,0 +1,30 @@
+// Creating own task and solving it
+
+// * Task: Drawing a cup of hot coffee
+
+// * Brainstorm
+
+/**
+ * A cup of coffee  
+ * A cup plate
+ *   Some curves (made os straight lines)
+ *   Semi Circle
+ * Steaming hot coffee
+ *   Some curves (made os straight lines)
+ */
+
+// * Specify
+
+/**
+ * Stage 1 (simplest): Drawing a stating cup of coffee on the cup plate and over them, static straights simulating the steam.
+ * 
+ * Stage 2 (builds on / extends Stage 1): Adding some solid color to the figures and.
+ * 
+ * Stage 3 (builds on / extends Stage 1): The steam of the hot coffee is not static, therefore, the steam have a wave animation.
+ */
+
+// * Design
+
+// * Implement
+
+// * Test
